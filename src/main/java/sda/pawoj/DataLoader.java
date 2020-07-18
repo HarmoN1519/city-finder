3c5305c31ac38e0084c62eabe03901aaa18bd392
PAWOJ <patryk95wojtow@gmail.com>
2020-07-18 10:36:52 +0200

    2nd commit

--- src/main/java/sda/pawoj/DataLoader.java
+++ src/main/java/sda/pawoj/DataLoader.java
@@ -1,5 +1,37 @@
 package sda.pawoj;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataLoader {
-    public static List<String>
+
+    public static List<String> loadFile(String path) throws FileNotFoundException {
+
+        List<String> loadedRows = new ArrayList<>();
+
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String line;
+        while (line = bufferedReader.readLine() != null) {
+            loadedRows.add(line);
+        }
+
+        try {
+            bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
